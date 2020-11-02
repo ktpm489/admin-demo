4b8ad00fb79a5ee6cfd0b072257d91c9e4b109b4
ktpm489 <ktpm489@gmail.com>
2020-11-02 11:14:12 +0700

    Update config3.js

--- config3.js
+++ config3.js
@@ -25,8 +25,8 @@
       5
    ],
    "configSkin": {
-      "email":'haravandemo@gmail.com',
-      "link" : 'https://shrouded-brushlands-68077.herokuapp.com/api/userskin',
-      'key' : 'NWY0N2FkMjg4ZjFiYmIwYWViZDBkNDdhXzU2Nzg5MTBfSG5mMlJRcDhMbkNuWWhBQw=='
-   },
+      "email":"haravandemo@gmail.com",
+      "link" : "https://shrouded-brushlands-68077.herokuapp.com/api/userskin",
+      "key" : "NWY0N2FkMjg4ZjFiYmIwYWViZDBkNDdhXzU2Nzg5MTBfSG5mMlJRcDhMbkNuWWhBQw=="
+   }
 }
