2ff2b297ba50c7fc912ef92087098e68d223e53f
ktpm489 <ktpm489@gmail.com>
2020-11-02 11:24:27 +0700

    Update config3.js

--- config3.js
+++ config3.js
@@ -1,29 +1,4 @@
 {
-   "anObject": {
-      "numericProperty": -122,
-      "stringProperty": "An offensive \" is problematic",
-      "nullProperty": null,
-      "booleanProperty": true,
-      "dateProperty": "2011-09-23"
-   },
-   "arrayOfObjects": [
-      {
-         "item": 1
-      },
-      {
-         "item": 2
-      },
-      {
-         "item": 3
-      }
-   ],
-   "arrayOfIntegers": [
-      1,
-      2,
-      3,
-      4,
-      5
-   ],
    "configSkin": {
       "email":"haravandemo@gmail.com",
       "link" : "https://shrouded-brushlands-68077.herokuapp.com/api/userskin",
