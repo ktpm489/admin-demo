b700ecea464764c417c6c7cfcbf893a8b8871c85
ktpm489 <ktpm489@gmail.com>
2021-03-31 10:10:07 +0700

    Update config3.js

--- config3.js
+++ config3.js
@@ -1,7 +1,7 @@
 {
    "configSkin": {
       "email":"haravandemo@gmail.com",
-      "link" : "https://shrouded-brushlands-68077.herokuapp.com/api/userskin",
+      "link" : "https://csgadmin.com/api/userskin",
       "key" : "NWY0N2FkMjg4ZjFiYmIwYWViZDBkNDdhXzU2Nzg5MTBfSG5mMlJRcDhMbkNuWWhBQw=="
    }
 }
